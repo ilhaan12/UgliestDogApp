35e3083a7667b2058807f38029bd264377d52fdb
ilhaan12 <168375285+ilhaan12@users.noreply.github.com>
2025-04-08 06:17:48 +0000

    Implement Ugliest Dogs page with images

--- UgliestDogApp/obj/Debug/net8.0/UgliestDogApp.AssemblyInfo.cs
+++ UgliestDogApp/obj/Debug/net8.0/UgliestDogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UgliestDogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6998d332f5721337df103e2d24aa56704114b722")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a187303dd6e3a73d94fb93eee07efa2cba9421")]
 [assembly: System.Reflection.AssemblyProductAttribute("UgliestDogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UgliestDogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
